fc92b707cca31ef27bcc5db56e64b3ccaf638f42
Mahd58 <m.mahd.khizer@gmail.com>
2025-08-21 16:53:36 +0500

    Add files via upload

--- tip.py
+++ tip.py
@@ -1,8 +1,44 @@
-# Let's create a function total_calc() that helps us calculate and print out the total amount paid at a restaurant. Given a bill amount and the percentage of the bill amount you decide to pay us a tip (tip_perc ), this function calculates the total amount you should pay.
-def total_calc(amount,tip_percentage):
-    tip=amount*tip_percentage*0.01
-    total=amount+tip
-    return total
-amount=int(input("Enter the pay:"))
-tip=int(input("Enter the tip:"))
-print(total_calc(amount,tip))
+# Write a program to calculate the total trip expenditure: Calculate the hotel cost per day Calculate the plane cost Price of the vehicle rented during the trip
+def planecost (city):
+    if city=="Karachi":
+        return 150
+    elif city=="Islamabad":
+        return 500
+    elif city=="Delhi":
+        return 1000
+    elif city=="Quetta":
+        return 200
+    elif city=="los angles":
+        return 10000
+    
+
+
+
+
+
+def carcost (days): 
+    if days>7:
+        return 100*days-50
+    elif days>3:
+        return 100*days-20
+    else:
+        return 100*days
+
+
+
+
+
+def hotelcost (night):
+    return 1000*night
+
+
+
+
+
+city=input("Enter your city:")
+day=int(input("Enter how many days you want to rent a car:"))
+nights=int(input("Enter how many nights you want to spent:"))
+
+print(planecost(city))
+print(carcost(day))
+print(hotelcost(nights))
